11519801a33a753f38b5139422e7e821c4aa14ed
Macomi27 <macomfortitcham@gmail.com>
2025-07-24 15:43:10 +0100

    Type Conversion

--- The JS Language/Type Conversions/main.js
+++ The JS Language/Type Conversions/main.js
@@ -11,6 +11,7 @@ let num = Number(str);
 // string   ->	Whitespaces (includes spaces, tabs \t, newlines \n etc.) from the start and end are removed. If the remaining string is empty, the result is 0. Otherwise, the number is “read” from the string. An error gives NaN.
 
 //Boolean Conversion
+value = Boolean(value);
 // The conversion rule:
 // Values that are intuitively “empty”, like 0, an empty string, null, undefined, and NaN, become false.
 // Other values become true.
