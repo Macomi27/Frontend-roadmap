3bbe78a4ca1ecdeffca9457c500b840439780b9c
Macomi27 <macomfortitcham@gmail.com>
2025-07-25 06:33:13 +0100

    Conditional Branching

--- The JS Language/Conditional Branching/main.js
+++ The JS Language/Conditional Branching/main.js
@@ -37,22 +37,56 @@ if (1) {
 // condition ? exprIfTrue : exprIfFalse;
 
 // A sequence of question mark operators ? can return a value that depends on more than one condition.
-let age = prompt("age?", 18);
+// let age = prompt("age?", 18);
 
-let message =
-  age < 3
-    ? "Hi, baby!"
-    : age < 18
-    ? "Hello!"
-    : age < 100
-    ? "Greetings!"
-    : "What an unusual age!";
+// let message =
+//   age < 3
+//     ? "Hi, baby!"
+//     : age < 18
+//     ? "Hello!"
+//     : age < 100
+//     ? "Greetings!"
+//     : "What an unusual age!";
 
-alert(message);
+// console.log(message);
 
 // The purpose of the question mark operator ? is to return one value or another depending on its condition. Use if to execute different branches of code.
 
-// Exercise: Will the result be shown?
+// Exercise1: Will the result be shown?
 if ("0") {
   console.log("Hello");
 } //yes
+
+//Exercise2
+// Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+// If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!”
+let officialName = prompt(`What is the "official" name of JavaScript?`, "");
+if ("ECMAScript") {
+  console.log("Right!");
+} else {
+  console.log(`You don't know? "ECMAScript`);
+}
+
+// Exercise 3: show the sign
+let number = prompt("Input a number", "");
+if (number > 0) {
+  alert(1);
+} else if (number < 0) {
+  alert(-1);
+} else if (number == 0) {
+  alert(0);
+}
+
+// Exercise 4 - Rewrite 'if' into '?'
+let result;
+a + b < 4 ? (result = "Below") : (result = "Over");
+
+// Exercise 5 - Rewrite 'if..else' into '?'
+let newMessage;
+login == Employee
+  ? (newMessage = "Hello")
+  : login == Director
+  ? (newMessage = "Greetings")
+  : login == ""
+  ? (newMessage = "No login")
+  : (newMessage = "");
