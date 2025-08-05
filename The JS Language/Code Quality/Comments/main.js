8bcc0236aa8e7fe08453a24acfe330073fa3dd10
Macomi27 <macomfortitcham@gmail.com>
2025-08-06 00:29:41 +0100

    Comments

---
+++ The JS Language/Code Quality/Comments/main.js
@@ -0,0 +1,9 @@
+// Comments - used to describe how and why the code works
+// comments can be single-line: starting with // and multiline: /* ... */
+
+// Bad comments - (explanatory comments) if the code is so unclear that it requires a comment, then maybe it should be rewritten instead
+
+// Good comments - (descriptive comments) describes th earchitecture, providing a high-level overview of components, how they interact, what the control flow is in every situation is...
+// UML aids to build high-level architecture diagrams and explaining the code.
+
+// Document function parameters and usage - JSDoc is a special syntax to document a function: usage, parameters, returned value.
