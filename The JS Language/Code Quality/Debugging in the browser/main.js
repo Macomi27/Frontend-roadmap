e6bb490ae287c51163759f47884400bd51bbdf77
Macomi27 <macomfortitcham@gmail.com>
2025-08-05 23:18:12 +0100

    Debugging in the browser

---
+++ The JS Language/Code Quality/Debugging in the browser/main.js
@@ -0,0 +1,27 @@
+// The Sources panel
+// The Sources panel has 3 parts:
+// The File Navigator pane lists HTML, JavaScript, CSS and other files, including images that are attached to the page. Chrome extensions may appear here too.
+// The Code Editor pane shows the source code.
+// The JavaScript Debugging pane is for debugging.
+
+// Breakpoints
+// To enable a breakpoint  click at line number in the Code Editor pane.
+
+// Code can also be paused using the command "debugger"
+
+// Tracing the execution
+//  – “Resume”: continue the execution, hotkey F8.
+//  – “Step”: run the next command, hotkey F9.
+//  – “Step over”: run the next command, but don’t go into a function, hotkey F10.
+//  – “Step into”, hotkey F11.
+//  – “Step out”: continue the execution till the end of the current function, hotkey Shift+F11.
+
+// Logging
+// The console.log() function is used to log messages to the console.
+
+// Summary
+// There are three main ways to pause a script:
+// A breakpoint.
+// The debugger statements.
+// An error (if dev tools are open and the button  is “on”).
+// When paused, we can debug: examine variables and trace the code to see where the execution goes wrong.
