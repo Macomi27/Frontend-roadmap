c3fac481e4714a880b9773aa51cedcb35202cc12
Macomi27 <macomfortitcham@gmail.com>
2025-08-06 00:48:19 +0100

    Ninja Code

---
+++ The JS Language/Code Quality/Ninja code/main.js
@@ -0,0 +1,36 @@
+// Ninja code - tricks to sharpen the mind of code maintainers.
+
+// Brevity is the soul of wit - Make the code as short as possible.
+
+// One-letter variables - using single-letter variable names everywhere. Like a, b or c.
+
+// Use abbreviations ex: list → lst; userAgent → ua; browser → brsr
+
+// Soar high. Be abstract.
+// The ideal name for a variable is data
+// Name a variable by its type:
+
+// Attention test
+// One of the ways – use similar variable names, like date and data
+
+// Smart synonyms
+// Using similar names for same things shows creativity to the public.
+// For instance, consider function prefixes. If a function shows a message on the screen – start it with display…, like displayMessage. And then if another function shows on the screen something else, like a user name, start it with show… (like showName).
+
+// Reuse names
+// Add a new variable only when absolutely necessary.
+
+// Underscores for fun
+// Put underscores _ and __ before variable names. Like _name or __value.
+// Let everyone see how magnificent your entities are! Names like superElement, megaFrame and niceItem will definitely enlighten a reader.
+
+// Overlap outer variables
+// Use same names for variables inside and outside a function. As simple. No efforts to invent new names.
+
+// Side-effects everywhere!
+// There are functions that look like they don’t change anything. Like isReady(), checkPermission(), findTags()… They are assumed to carry out calculations, find and return the data, without changing anything outside of them. In other words, without “side-effects”.
+// A really beautiful trick is to add a “useful” action to them, besides the main task.
+
+// Powerful functions!
+// Don’t limit the function by what’s written in its name. Be broader.
+// For instance, a function validateEmail(email) could (besides checking the email for correctness) show an error message and ask to re-enter the email.
