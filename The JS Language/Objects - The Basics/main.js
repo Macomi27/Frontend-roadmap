9ca2bbcf264a8e95d8fde68f92f23ff27e4830aa
Macomi27 <macomfortitcham@gmail.com>
2025-08-07 19:28:22 +0100

    Objects definition and syntax

---
+++ The JS Language/Objects - The Basics/main.js
@@ -0,0 +1,12 @@
+// Objects
+// An object can be created with figure brackets {…} with an optional list of properties. A property is a “key: value” pair, where key is a string (also called a “property name”), and value can be anything.
+
+// syntax:
+// let user = new Object(); // "object constructor" syntax
+// let user = {};  // "object literal" syntax
+
+// A property has a key (also known as “name” or “identifier”) before the colon ":" and a value to the right of it.
+
+// To remove a property, use the delete operator: delete user.age;
+
+// multiword property names, but then they must be quoted:
